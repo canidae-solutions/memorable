3d1f5cd9e5c27a8ee695b622bbb8691677d7476f
Ruby Iris Juric <ruby@srxl.me>
2025-03-20 00:04:56 +1100

    Improve logging/error handling in init_db

--- lib/mix/memorable.initdb.ex
+++ lib/mix/memorable.initdb.ex
@@ -1,20 +1,58 @@
 defmodule Mix.Tasks.Memorable.InitDb do
   @shortdoc "Initializes the Memorable database"
 
+  @tables [
+    Memorable.Collection
+  ]
+
   use Mix.Task
   require Logger
 
   @impl Mix.Task
   def run(_) do
-    nodes = [node()]
-    Memento.stop()
-
-    with :ok <- Memento.Schema.create(nodes),
-         _ <- Memento.start(),
-         :ok <- Memento.Table.create(Memorable.Collection, disc_copies: nodes) do
+    with :ok <- create_schema(),
+         :ok <- create_tables(@tables) do
       Logger.info("Memorable database initialized")
     else
-      {:error, err} -> Logger.error("Error initializing Memorable database: #{inspect(err)}")
+      {:error, _} -> Logger.error("Memorable database initialization failed")
+    end
+  end
+
+  defp create_schema() do
+    Memento.stop()
+
+    case Memento.Schema.create([node()]) do
+      :ok ->
+        Logger.info("Created schema for node #{node()}")
+        Memento.start()
+        :ok
+
+      {:error, {_, {:already_exists, _}}} ->
+        Logger.debug("Schema for node #{node()} already exists, skipping")
+        Memento.start()
+        :ok
+
+      {:error, err} ->
+        Logger.error("Error creating schema: #{inspect(err)}")
+        {:error, err}
+    end
+  end
+
+  defp create_tables([]), do: :ok
+
+  defp create_tables([table | rest]) do
+    case Memento.Table.create(table, disc_copies: [node()]) do
+      :ok ->
+        Logger.info("Created table #{inspect(table)}")
+        create_tables(rest)
+
+      {:error, {:already_exists, _}} ->
+        Logger.debug("Table #{inspect(table)} already exists, skipping")
+        create_tables(rest)
+
+      {:error, err} ->
+        Logger.error("Error creating table #{inspect(table)}: #{inspect(err)}")
+        {:error, err}
     end
   end
 end
